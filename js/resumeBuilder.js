86c80d78046ec32f876cb6bf639cd0f53d1f1811
Krittiya Clark <krittiyaclark@gmail.com>
2018-07-01 00:07:55 -0400

    Object structure

---
+++ js/resumeBuilder.js
@@ -0,0 +1,55 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+(function () 
+	let bio = {
+	 	name: 'Krittiya Clark',
+	    role: 'Front End Software Engineer',
+	    contacts : an object with
+	          mobile: string
+	          email: string 
+	          github: string
+	          twitter: string (optional)
+	          location: string
+	    welcomeMessage: string 
+	    skills: array of strings
+	    biopic: string url
+	    display: function taking no parameters
+ }
+
+	let education = {
+		schools: array of objects with
+         name: string
+         location: string
+         degree: string
+         majors: array of strings
+         dates: string (works with a hyphen between them)
+         url: string (optional)
+    onlineCourses: array of objects with
+         title: string
+         school: string
+         dates: string (works with a hyphen between them)
+         url: string
+    display: function taking no parameters
+ }
+
+	let work = {
+		jobs: array of objects with
+         employer: string 
+         title: string 
+         location: string 
+         dates: string (Can be 'in progress')
+         description: string 
+    display: function taking no parameters
+ }
+
+	let projects = {
+		projects: array of objects with
+          title: string 
+          dates: string (works with a hyphen between them)
+          description: string
+          images: array with string urls
+    display: function taking no parameters
+ }
+})();
